3aaa3bbb94d8357473f924a5f3ac56e5d7692e7d
DBappa <bappadityadutta2024@gmail.com>
2024-05-24 08:56:54 +0530

    Type Conversion and Coersion

---
+++ Fundamentals/TypeConversion.js
@@ -0,0 +1,31 @@
+
+
+//Type Conversion Examples
+
+const inputYear='1990';
+
+console.log(Number(inputYear));
+
+console.log(inputYear+20);
+
+console.log(Number(inputYear)+20);
+
+console.log(Number('Bappa'));
+
+console.log(typeof(NaN))
+
+
+//Type Coersion
+
+console.log('I am '+ 23 +' Years old');
+
+console.log('23' -'10' + 5);
+
+console.log('27'/'2');
+
+//Guess the output
+let n ='1'+ 1; 
+n = n - 1;
+console.log(n);
+
+console.log(2+3+4+'5');
